41e992f87146ca5f1b20dd7d08123cfddc295f91
Denis Kolosov <denis.kolosov@gmail.com>
2017-04-10 07:41:02 +0300

    Change settings

--- weather/settings.py
+++ weather/settings.py
@@ -129,15 +129,15 @@ USE_TZ = True
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'assets'),
+    os.path.join(BASE_DIR, 'assets'),
 )
-
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'bundles/',
