00ea980b288636b0123700613cb4f3da6f327178
Denis Kolosov <denis.kolosov@gmail.com>
2017-04-10 08:10:18 +0300

    Change run script

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+npm install
 ./node_modules/.bin/webpack --config webpack.config.js
 if [ ! -e weatherdb.sqlite3 ]; then
     touch weatherdb.sqlite3
 fi
 
+python manage.py collectstatic
 python manage.py runserver 8000
