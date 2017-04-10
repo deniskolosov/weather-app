e5a6dd0651a7cbf95aec9f89bbd46792fd6c6f7a
Denis Kolosov <denis.kolosov@gmail.com>
2017-04-10 07:24:17 +0300

    Debug = False

--- weather/settings.py
+++ weather/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -137,7 +137,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'assets'),
 )
-
 
 WEBPACK_LOADER = {
     'DEFAULT': {
